09164659c59e8b6182b9366400f94fdceb90cd0f
yribarne <ramon.yribarnegaray@gmail.com>
2016-04-01 13:36:41 -0400

    add /resume

--- js/script.js
+++ js/script.js
@@ -1,42 +1,18 @@
-// var message="in global";
-// console.log (message);
-// var a= function() {
-// 	var message="inside function";
-// 	console.log(message);
-// }
 
-// function Test (h,w){
-// 	this.height = h;
-// 	this.width = w;
 
-// }
-// Test.prototype.area= function (){
-// 	return this.height*this.width;
-// }
-// var mySquare= new Test(3,4);
-
-// testObject={
-// 	name : "Ramon",
-// 	sayName: function(){
-// 		console.log("Hello "+this.name)
-// 	}
-
-// };
-
-// testObject.sayName();
 function showList(event){
 	var clickedButton=this.id;
 	console.log(clickedButton);
 	var newSelector=clickedButton.replace("b","l");
 	var newClass="";
-	var newText="MORE";
-	if (this.textContent=="MORE") {
+	var newText="MORE INFO";
+	if (this.textContent=="MORE INFO") {
 		newClass="";
 		newText="LESS";
 	}
 	else {
-		newClass="hidden-xs";
-		newText="MORE";		
+		newClass="hidden-xs hidden-sm";
+		newText="MORE INFO";		
 	}
 	document.querySelector("#"+newSelector).className=newClass;
 	this.textContent=newText;
